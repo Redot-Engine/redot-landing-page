2cca6402ef5a473ac1a176c0397b4c3366ff0b93
YanLi-FE <70615457+YanLi-FE@users.noreply.github.com>
2024-10-09 08:14:30 +0600

    fix: remove accidental inclusion of the image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
-  modules: ["@nuxt/content", "@nuxt/image"],
+  modules: ["@nuxt/content"],
   vite: {
     css: {
       preprocessorOptions: {
