c95d6bffef86e3df5805b849254c5068ac4f0779
Arjan Dikhoff <dikhoff@gmail.com>
2024-10-13 18:33:03 +0200

    Correct link

--- composables/useLinks.ts
+++ composables/useLinks.ts
@@ -5,5 +5,5 @@ export const useLinks = () => ({
   twitterUrl: "https://x.com/redotengine",
   redditUrl: "https://www.reddit.com/r/RedotGameEngineMain/",
   pressKit: "https://cdn.discordapp.com/attachments/1290096105094385695/1292789668907057292/Redot_Engine_-_Brand_Kit_v.1FINAL.zip?ex=6708f833&is=6707a6b3&hm=8d631f74ebb4bfc0e0b5de3989559a75820d42620944a2cc794ba4b60fbedaff&",
-  documentation: "https://docs.godotengine.org/",
+  documentation: "https://docs.redotengine.org/",
 });
