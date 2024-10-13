f1b4e181f95e6766d1922609e22bdc10ec76e3b4
Arjan Dikhoff <dikhoff@gmail.com>
2024-10-13 19:03:11 +0200

    Fix presskit link

--- composables/useLinks.ts
+++ composables/useLinks.ts
@@ -4,6 +4,6 @@ export const useLinks = () => ({
   discordUrl: "https://discord.gg/redot",
   twitterUrl: "https://x.com/redotengine",
   redditUrl: "https://www.reddit.com/r/RedotGameEngineMain/",
-  pressKit: "https://cdn.discordapp.com/attachments/1290096105094385695/1292789668907057292/Redot_Engine_-_Brand_Kit_v.1FINAL.zip?ex=6708f833&is=6707a6b3&hm=8d631f74ebb4bfc0e0b5de3989559a75820d42620944a2cc794ba4b60fbedaff&",
+  pressKit: "https://download.redotengine.org/Redot%20Engine%20-%20Brand%20Kit%20(v.1FINAL).zip",
   documentation: "https://docs.redotengine.org/",
 });
