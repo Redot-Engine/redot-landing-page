b7d6458e6cea78db7d418c632791f33f5aa755da
chrisrabe <chris.rabe14@gmail.com>
2024-10-15 07:33:52 +0800

    feat: moved news overview into components folder

---
+++ components/NewsPage/NewsOverviewPage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const tags = ref(["All", "News", "Releases", "Community"]);
+
+</script>
+
+<template>
+  <div class="chips-row">
+    <Chip
+      v-for="tag in tags"
+      :key="tag"
+      :href="`/news?tag=${tag}`"
+      as-button
+    >
+      {{ tag }}
+    </Chip>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.chips-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+</style>
