72d57e45373c7e2209d765de3ad5eb5fdb475937
chrisrabe <chris.rabe14@gmail.com>
2024-10-15 09:38:04 +0800

    fix: undo sorting change

--- components/NewsPage/NewsOverviewPage.vue
+++ components/NewsPage/NewsOverviewPage.vue
@@ -4,7 +4,7 @@ import NewsOverviewPageArticle from "~/components/NewsPage/NewsOverviewPageArtic
 const { data: posts } = await useAsyncData("recent-posts", () =>
     queryContent("/news")
         .where({ type: { $eq: "post" }, published: { $eq: true } })
-        .sort({ date: -1 })
+        .sort({ date: -1, index: 1 })
         .find(),
 );
 </script>
