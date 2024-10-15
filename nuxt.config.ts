0163b1e15343c4d4781a62ee731850d3f658c3e0
Reuben Tier <otterlord.dev@gmail.com>
2024-10-15 15:49:11 +0100

    Add avif support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   modules: ["@nuxt/content", "@nuxt/image", "@nuxtjs/sitemap"],
+  image: {
+    format: ["avif", "webp"],
+    quality: 80,
+  },
   site: {
     url: "https://www.redotengine.org/",
     name: "Redot Engine: Open source game engine for everyone.",
