d4e5ca8dd8a20eb19040c2fd79dbe543ace1fed3
Arjan Dikhoff <dikhoff@gmail.com>
2024-10-15 05:32:04 +0200

    Remove nuxt preload code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  head: {
-    link: [
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_01.avif" },
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_02.avif" },
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_03.avif" },
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_05.png" },
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_06.png" },
-      { rel: "preload", as: "image", href: "~/assets/images/game_preview/game_preview_07.png" },
-    ],
-  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
