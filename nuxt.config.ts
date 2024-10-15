91322f7cc6b3902d70003f67bce269d59196a740
Victor Chanil Park <opdev1004@gmail.com>
2024-10-15 23:13:31 +0900

    feat: sitemap generation (#70)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: true,
-  modules: ["@nuxt/content"],
+  site: {
+    url: "https://www.redotengine.org/",
+    name: "Redot Engine: Open source game engine for everyone.",
+  },
+  modules: ["@nuxt/content", "@nuxtjs/sitemap"],
   vite: {
     css: {
       preprocessorOptions: {
