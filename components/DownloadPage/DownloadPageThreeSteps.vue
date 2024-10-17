68a16f4c7069bae3333b6bb6508a7ee3e60b3a8c
Charlotte <charlottewiltshire27@outlook.com>
2024-10-17 17:53:17 +0300

    feat: add DownloadPageThreeSteps and DownloadPageThreeStepsItem components

---
+++ components/DownloadPage/DownloadPageThreeSteps.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+  <section class="download-three-steps">
+    <div class="download-three-steps__content">
+      <span class="download-three-steps__title">Ignite Your Imagination</span>
+      <div class="download-three-steps__grid">
+        <DownloadPageThreeStepsItem
+          :links="[
+            { text: 'Visit the download archive', url: 'https://github.com/Redot-Engine/redot-engine/releases' },
+          ]"
+          description="You can choose the latest Redot Engine, an older release, or try a beta version."
+          title="1. Choose Your Redot Version"
+        />
+        <DownloadPageThreeStepsItem
+          :links="[
+            { text: 'Download for Windows', url: 'https://download.redotengine.org/release/4.4/redot-windowsx64.zip' },
+            { text: 'Download for Linux', url: 'https://download.redotengine.org/release/4.4/redot-linuxbsd.zip' },
+            { text: 'Download for Android', url: 'https://download.redotengine.org/release/4.4/android-editor.zip' },
+          ]"
+          description="Follow the instructions to complete installation and setup."
+          title="2. Download Redot Engine"
+        />
+        <DownloadPageThreeStepsItem
+          :links="[
+            { text: 'Join Our Discord Community', url: 'https://discord.gg/redot' },
+          ]"
+          description="Start your project from scratch or choose a template."
+          title="3. Start Your Project"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/mixins";
+
+.download-three-steps {
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    row-gap: 2.25rem;
+    overflow: hidden;
+  }
+
+  &__title {
+    font-weight: 800;
+    font-size: 2.25rem;
+    letter-spacing: -1px;
+    line-height: 2.5rem;
+  }
+
+  @media (max-width: 768px) {
+    &__title {
+      font-size: 1.5rem;
+      line-height: 2rem;
+      letter-spacing: 0;
+    }
+  }
+
+  &__grid {
+    max-width: 1024px;
+    padding-right: 2rem;
+    padding-left: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    gap: 1rem;
+    justify-items: center;
+    height: 100%;
+    grid-column: span 12 / span 12;
+
+    @media (min-width: 768px) {
+      grid-column: span 6 / span 6;
+      gap: 2rem
+    }
+
+    @media (min-width: 1024px) {
+      grid-column: span 4 / span 4;
+    }
+  }
+}
+</style>
---
+++ components/DownloadPage/DownloadPageThreeStepsItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="download-three-steps-item">
+    <div class="download-three-steps-item__content">
+      <span class="download-three-steps-item__title">{{ title }}</span>
+      <div class="download-three-steps-item__details">
+        <p class="download-three-steps-item__description">{{ description }}</p>
+        <div>
+          <div v-for="(link, index) in links" :key="index">
+            <NuxtLink :to="link.url" class="download-three-steps-item__link">{{ link.text }}</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const { title, description, links } = defineProps<{
+  title: string;
+  description: string;
+  links: { text: string; url: string }[];
+}>();
+</script>
+
+<style scoped lang="scss">
+@use "@/assets/styles/mixins";
+
+.download-three-steps-item {
+  &__content {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+    overflow: hidden;
+    max-width: 18rem;
+  }
+
+  @media (max-width: 768px) {
+    &__content {
+      max-width: 30rem;
+    }
+  }
+
+  &__title {
+    font-weight: 800;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+  }
+
+  &__description {
+    font-weight: 200;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: rgba(#fff, 80%);
+  }
+
+  &__details {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.625rem;
+  }
+
+  &__link {
+    font-weight: 200;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: #3388ff;
+    text-decoration: none;
+  }
+
+  &__link:hover {
+    text-decoration: underline;
+  }
+}
+
+</style>
