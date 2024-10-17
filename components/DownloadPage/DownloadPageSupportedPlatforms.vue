a49b61edd35ca176d400c1886d6cca2eed91abe2
Charlotte <charlottewiltshire27@outlook.com>
2024-10-17 20:31:19 +0300

    feat: add supported platforms section with individual platform items

---
+++ components/DownloadPage/DownloadPageSupportedPlatforms.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="download-supported-platforms">
+    <div class="download-supported-platforms__content">
+      <span class="download-supported-platforms__title">
+        Supported Platforms
+      </span>
+      <div class="download-supported-platforms__grid">
+        <DownloadPageSupportedPlatformsItem
+          class="download-supported-platforms__android"
+          icon="/img/download/android.svg"
+          label="Android"
+        />
+        <DownloadPageSupportedPlatformsItem
+          class="download-supported-platforms__web-editor"
+          icon="/img/download/web-editor.svg"
+          label="Web Editor"
+        />
+        <DownloadPageSupportedPlatformsItem
+          class="download-supported-platforms__windows"
+          icon="/img/download/windows.svg"
+          label="Windows"
+        />
+        <DownloadPageSupportedPlatformsItem
+          class="download-supported-platforms__mac"
+          icon="/img/download/apple.svg"
+          label="macOS"
+        />
+        <DownloadPageSupportedPlatformsItem
+          class="download-supported-platforms__linux"
+          icon="/img/download/linux.svg"
+          label="Linux"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+$grid-columns: 5;
+$gap: 20px;
+
+@mixin grid-item {
+  flex: 1 0 auto;
+  text-align: center;
+  padding: $gap / 2;
+
+  img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+  }
+}
+
+.download-supported-platforms {
+  &__android {
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(23, 254, 2, 0.3) 194%
+    );
+  }
+  &__web-editor {
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgb(254, 63, 2, 0.3) 200%
+    );
+  }
+  &__windows {
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgb(2, 178, 254, 0.3) 194%
+    );
+  }
+  &__mac {
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(254, 174, 2, 0.3) 194%
+    );
+  }
+  &__linux {
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(255, 255, 255, 0.3) 194%
+    );
+  }
+
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    row-gap: 2.25rem;
+    overflow: hidden;
+  }
+
+  &__title {
+    font-weight: 800;
+    font-size: 2.25rem;
+    letter-spacing: -1px;
+    line-height: 2.5rem;
+  }
+
+  @media (max-width: 768px) {
+    &__title {
+      font-size: 1.5rem;
+      line-height: 2rem;
+      letter-spacing: 0;
+    }
+  }
+
+  &__grid {
+    max-width: 1024px;
+    padding: 0 4rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+    height: 100%;
+    grid-column: span 12 / span 12;
+
+    @media (min-width: 768px) {
+      grid-column: span 6 / span 6;
+      gap: 2rem;
+      width: 100%;
+    }
+
+    @media (min-width: 1024px) {
+      grid-column: span 4 / span 4;
+    }
+  }
+}
+</style>
---
+++ components/DownloadPage/DownloadPageSupportedPlatformsItem.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+const {
+  icon,
+  label,
+  href,
+  class: className,
+} = defineProps<{
+  label: string;
+  href: string;
+  icon?: string;
+  class?: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink :class="['link-button', className]" :href="href">
+    <img v-if="icon" :src="icon" alt="" class="link-button__icon" />
+    <span class="link-button__label">{{ label }}</span>
+  </NuxtLink>
+</template>
+
+<style scoped lang="scss">
+.link-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1.25rem 1.875rem;
+  text-align: center;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 12px;
+  color: #fff;
+  transition: background-color 0.3s;
+
+  &__icon {
+    margin-right: 1rem;
+    width: 32px;
+    height: 32px;
+  }
+
+  &__label {
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
+
+  @media (max-width: 768px) {
+    &__label {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+    }
+  }
+
+  &:hover {
+    background-color: rgba(#fff, 5%);
+  }
+}
+
+@media (max-width: 768px) {
+  .link-button {
+    width: 100%;
+  }
+}
+</style>
