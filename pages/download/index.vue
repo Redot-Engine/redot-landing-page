f339a836ed1909e5763a854a9a4553f18fe88054
Charlotte <charlottewiltshire27@outlook.com>
2024-10-17 19:02:19 +0300

    feat: add download redirection and platform-specific download page

---
+++ pages/download/index.vue
@@ -0,0 +1,30 @@
+<template></template>
+
+<script>
+export default {
+  mounted() {
+    this.redirectToPlatform();
+  },
+  methods: {
+    redirectToPlatform() {
+      const userAgent = navigator.userAgent.toLowerCase();
+      let platform;
+
+      if (userAgent.includes("win")) {
+        platform = "windows";
+      } else if (userAgent.includes("mac")) {
+        platform = "mac";
+      } else if (userAgent.includes("linux")) {
+        platform = "linux";
+      } else {
+        platform = "other";
+      }
+
+      this.$router.push(`/download/${platform}`);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+@use "@/assets/styles/mixins";
+</style>
