8a68cf42917e98b46015da65e80af3c1bffbe5eb
Charlotte <charlottewiltshire27@outlook.com>
2024-10-18 10:51:39 +0300

    refactor: Update platform redirection logic in download page

--- pages/download/index.vue
+++ pages/download/index.vue
@@ -1,30 +1,29 @@
-<template></template>
+<template>
+  <div />
+</template>
 
-<script>
-export default {
-  mounted() {
-    this.redirectToPlatform();
-  },
-  methods: {
-    redirectToPlatform() {
-      const userAgent = navigator.userAgent.toLowerCase();
-      let platform;
+<script setup>
+const router = useRouter();
+const redirectToPlatform = () => {
+  const userAgent = navigator.userAgent.toLowerCase();
+  let platform;
 
-      if (userAgent.includes("win")) {
-        platform = "windows";
-      } else if (userAgent.includes("android")) {
-        platform = "android";
-      } else if (userAgent.includes("linux")) {
-        platform = "linux";
-      } else {
-        platform = "other";
-      }
+  if (userAgent.includes("win")) {
+    platform = "windows";
+  } else if (userAgent.includes("android")) {
+    platform = "android";
+  } else if (userAgent.includes("linux")) {
+    platform = "linux";
+  } else {
+    platform = "linux";
+  }
 
-      this.$router.push(`/download/${platform}`);
-    },
-  },
+  router.push(`/download/${platform}`);
 };
+
+onBeforeRouteUpdate(redirectToPlatform);
 </script>
+
 <style scoped lang="scss">
 @use "@/assets/styles/mixins";
 </style>
