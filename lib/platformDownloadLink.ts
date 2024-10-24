b1714143b6d441b93082274adc04bb81def889ba
Craptain <dikhoff@gmail.com>
2024-10-24 04:51:29 +0200

    Change links to version 4.3 beta (#142)

--- lib/platformDownloadLink.ts
+++ lib/platformDownloadLink.ts
@@ -1,10 +1,9 @@
 export const getPlatformDownloadLink = (platform: string): string => {
   const downloadLinks: { [key: string]: string } = {
-    windows:
-      "https://download.redotengine.org/release/4.4/redot-windowsx64.zip",
-    linux: "https://download.redotengine.org/release/4.4/redot-linuxbsd.zip",
+    windows: "https://download.redotengine.org/release/4.3/Redot_v4.3-beta.1_win64.exe.zip",
+    linux: "https://download.redotengine.org/release/4.3/Redot_v4.3-beta.1_linux.x86_64.zip",
     android: "https://download.redotengine.org/release/4.4/android-editor.zip",
-    mac: "https://github.com/Redot-Engine/redot-engine/releases",
+    mac: "https://download.redotengine.org/release/4.3/Redot_v4.3-beta.1_macos.zip",
   };
 
   return (
