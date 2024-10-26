9cf9636725c2bd577572c6cab0a38efe1fa14358
Chris Rabe <chris.rabe14@gmail.com>
2024-10-26 20:23:27 +0800

    148 create a license page and centralised copyright file for third party assets (#150)
    * feat: added license page
    
    * feat: added license page content

---
+++ pages/license/index.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const { data } = await useAsyncData("license", () => (
+    queryContent("/license").where({ type: { $eq: "license" } }).findOne()
+));
+
+useHead(data.value?.title ? {
+  title: data.value?.title,
+}: {});
+</script>
+
+<template>
+  <Header always-opaque />
+  <MaxWidthContainer class="license-page-container">
+    <div class="header">
+      <h1 class="section-title-text">License</h1>
+    </div>
+    <LicensePageContent :body="data?.body!" />
+  </MaxWidthContainer>
+  <Footer />
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/mixins";
+
+.license-page-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  margin-top: 56px;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.section-title-text {
+  display: inline-block;
+  font-size: 36px;
+  font-weight: 800;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 4px;
+    left: 0;
+    right: 0;
+    height: 10px;
+    background-color: #fe3f02;
+    z-index: -1;
+  }
+
+  @include mixins.mobile-and-smaller {
+    font-size: 24px;
+
+    &::before {
+      height: 6px;
+    }
+  }
+}
+</style>
