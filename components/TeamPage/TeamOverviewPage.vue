5b7f0c5221129d21e5cb23ad3dec59c5c4cf06a1
Charlotte <charlottewiltshire27@outlook.com>
2024-10-28 21:52:48 +0200

    feat: add TeamOverviewPage and TeamOverviewPageUserCard components for team member display

---
+++ components/TeamPage/TeamOverviewPage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { team } from "~/data/team";
+</script>
+
+<template>
+  <section class="team-overview">
+    <div v-for="(member, index) in team" :key="index" class="team-overview__member">
+      <TeamPageTeamOverviewPageUserCard
+        :avatar-url="member.avatarUrl"
+        :display-name="member.displayName"
+        :user-role="member.userRole"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.team-overview {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+
+  &__member {
+    max-width: 250px;
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
---
+++ components/TeamPage/TeamOverviewPageUserCard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { TeamMember } from "~/types/TeamMember";
+
+const { avatarUrl, displayName, userRole } = defineProps<TeamMember>();
+</script>
+
+<template>
+  <div class="user-card">
+    <img :alt="displayName" :src="avatarUrl" class="user-card__avatar">
+    <div class="user-card__info">
+      <span class="user-card__name">{{ displayName }}</span>
+      <span class="user-card__role">{{ userRole }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 1rem;
+
+  &__avatar {
+    width: 125px;
+    height: 125px;
+    border-radius: 10%;
+    margin-bottom: 1rem;
+  }
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__name {
+    font-size: 1.25rem;
+    font-weight: bold;
+    margin-bottom: 0.4rem;
+    word-break: break-word;
+  }
+
+  &__role {
+    font-size: 0.875rem;
+    color: rgba(#fff, 60%);
+  }
+}
+</style>
