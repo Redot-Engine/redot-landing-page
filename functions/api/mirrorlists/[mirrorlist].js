dff7ece1f6ea34da767666945bdd3c8e450e032d
Victor Chanil Park <opdev1004@gmail.com>
2024-11-04 13:13:13 +0900

    fix: mirrorlist download  tag

--- functions/api/mirrorlists/[mirrorlist].js
+++ functions/api/mirrorlists/[mirrorlist].js
@@ -1,11 +1,12 @@
 export async function onRequest(context)
 {
     const version = context.params.mirrorlist.replace('.json', '');
+    const downloadPathVersion = version.replace(/\./g, '-');
     const mirrorlist = {
         mirrors: [
             {
                 name: "Official GitHub Releases mirror",
-                url: `https://github.com/Redot-Engine/redot-engine/releases/download/redot-${ version }/Redot_v${ version.replace(/-/g, '_') }_export_templates.tpz`
+                url: `https://github.com/Redot-Engine/redot-engine/releases/download/redot-${ downloadPathVersion }/Redot_v${ version }_export_templates.tpz`
             }
         ]
     };
