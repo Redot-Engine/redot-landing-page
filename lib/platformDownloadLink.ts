a65be49b176c36dec41fb4a299fb5028c40b5c80
Spartan322 <Megacake1234@gmail.com>
2024-11-13 14:09:15 -0500

    feat: add release notes for Redot 4.3 RC3

--- lib/platformDownloadLink.ts
+++ lib/platformDownloadLink.ts
@@ -1,9 +1,9 @@
 export const getPlatformDownloadLink = (platform: string): string => {
   const downloadLinks: { [key: string]: string } = {
-    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_win64.exe.zip",
-    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_linux.x86_64.zip",
-    android: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_android_editor.apk",
-    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_macos.zip",
+    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_win64.exe.zip",
+    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_linux.x86_64.zip",
+    android: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_android_editor.apk",
+    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_macos.zip",
   };
 
   return (
