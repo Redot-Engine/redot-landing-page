a3d9a61fa69b9a7c3a32d0ae36699bbd9b7ba131
Andrew Martin <andrewmartin@Andrews-MacBook-Pro.local>
2024-11-13 15:43:21 -0600

    fixed windows link

--- lib/platformDownloadLink.ts
+++ lib/platformDownloadLink.ts
@@ -1,9 +1,10 @@
 export const getPlatformDownloadLink = (platform: string): string => {
   const downloadLinks: { [key: string]: string } = {
-    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_windows_arm64.exe.zip",
+    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_win64.exe.zip",
     linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_linux.x86_64.zip",
     android: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_android_editor.apk",
     mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.2/Redot_v4.3-rc.2_macos.zip",
+
   };
 
   return (
