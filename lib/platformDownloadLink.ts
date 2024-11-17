73ab4181c7ba9e19611be6d34c318aebdd1b7f16
Spartan322 <Megacake1234@gmail.com>
2024-11-16 20:02:01 -0500

    feat: add release notes for Redot 4.3 RC4

--- lib/platformDownloadLink.ts
+++ lib/platformDownloadLink.ts
@@ -1,9 +1,9 @@
 export const getPlatformDownloadLink = (platform: string): string => {
   const downloadLinks: { [key: string]: string } = {
-    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_win64.exe.zip",
-    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_linux.x86_64.zip",
-    android: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_android_editor.apk",
-    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_macos.zip",
+    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_win64.exe.zip",
+    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_linux.x86_64.zip",
+    android: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_android_editor.apk",
+    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_macos.zip",
   };
 
   return (
@@ -14,9 +14,9 @@ export const getPlatformDownloadLink = (platform: string): string => {
 
 export const getMonoPlatformDownloadLink = (platform: string): string => {
   const downloadLinks: { [key: string]: string } = {
-    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_mono_win64.zip",
-    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_mono_linux_x86_64.zip",
-    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.3/Redot_v4.3-rc.3_mono_macos.zip",
+    windows: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_mono_win64.zip",
+    linux: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_mono_linux_x86_64.zip",
+    mac: "https://github.com/Redot-Engine/redot-engine/releases/download/redot-4.3-rc.4/Redot_v4.3-rc.4_mono_macos.zip",
   };
 
   return (
